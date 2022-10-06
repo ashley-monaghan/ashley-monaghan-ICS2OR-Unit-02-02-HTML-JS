7f9246ad9c7ef8ef748a70287cb78b8b158ada5a
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-06 14:41:38 -0400

    The actual work

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Ashley Monaghan All rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+ function doMathClicked() {
+    document.getElementById("the-area").innerHTML = "<p>?" + (5 * 3) + "</p>"
+    document.getElementById("the-perimiter").innerHTML = "<p>?" + ((5*2)+(3*2)) + "</p>"
